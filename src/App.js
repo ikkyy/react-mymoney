8111c565ce0df7338ce2f97174c680416ac74bc7
Ikkyy <msndoikky@hotmail.com>
2019-08-26 13:38:49 -0300

    CRUD

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import useGet from './useGet'
+import usePost from './usePost'
+import useDelete from './usePost'
+
+const url = 'https://mymoney-devpleno2.firebaseio.com/movimentacoes/2019-08.json'
 
 function App() {
+
+  const data = useGet(url)
+  const [postData, post] = usePost(url)
+  const [deleteData, remove] = useDelete()
+  const saveNew = () => {
+    post({ valor: 10, descricao: "olá" })
+  }
+  const doRemove = () => {
+    remove('https://mymoney-devpleno2.firebaseio.com/movimentacoes/2019-08/-LnDiS_P_AC8Kj210FvF')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>MyMoney</h1>
+      {JSON.stringify(data)}
+      {data.loading && <p>Loading...</p>}
+      <button onClick={saveNew}>Salvar</button>
+      <pre>{JSON.stringify(postData)}</pre>
+      <button onClick={doRemove}>Remover</button>
+      <pre>{JSON.stringify(deleteData)}</pre>
+
+
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/usePost.js
@@ -0,0 +1,41 @@
+import { useReducer, useEffect } from 'react'
+import axios from 'axios'
+
+const reducer = (state, action) => {
+    if (action.type === 'REQUEST') {
+        return {
+            ...state,
+            loading: false
+        }
+    }
+    if (action.type === 'SUCCESS') {
+        return {
+            ...state,
+            loading: false,
+            data: action.data
+        }
+    }
+    return state
+}
+
+const usePost = (url) => {
+    const [data, dispatch] = useReducer(reducer, {
+        loading: true,
+        data: {}
+    })
+    const post = data => {
+        dispatch({type: 'REQUEST'})
+        axios
+            .post(url, data)
+            .then(res => {
+                dispatch({
+                    type: 'SUCCESS',
+                    data: res.data
+                })
+                console.log(res.data)
+            })
+    }
+    return [data, post]
+}
+
+export default usePost
