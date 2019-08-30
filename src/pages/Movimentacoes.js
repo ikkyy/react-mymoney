9f11c657089ad559b455e6943cbfb4da50e548de
Ikkyy <msndoikky@hotmail.com>
2019-08-30 14:19:40 -0300

    Movimentacoes

--- src/pages/Movimentacoes.js
+++ src/pages/Movimentacoes.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import Rest from '../utils/rest'
 
-const Movimentacoes = () => {
+const baseURL = 'https://mymoney-devpleno2.firebaseio.com/'
+const { useGet } = Rest(baseURL)
+
+
+const Movimentacoes = ({ match }) => {
+    const data = useGet(`movimentacoes/${match.params.data}`)
     return (
-        <h1>Movimentações</h1>
+        <div className='container'>
+            <h1>Movimentações</h1>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Descrição</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.data &&
+                        Object
+                            .keys(data.data)
+                            .map(movimentacao => {
+                                return (
+                                    <tr>
+                                        <td>{data.data[movimentacao].descricao}</td>
+                                        <td>{data.data[movimentacao].valor}</td>
+                                    </tr>
+                                )
+                            })
+                    }
+
+                </tbody>
+            </table>
+        </div>
     )
 }
 
