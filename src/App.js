a8ea1a660702991631a0efa72a282e34e120a947
Ikkyy <msndoikky@hotmail.com>
2019-08-30 14:07:40 -0300

    React-router-dom

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
 import React from 'react'
-import Rest from './rest'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './elements/Header'
-import Meses from './Meses'
-import AdicionarMes from './AdicionarMes'
-
-const baseURL = 'https://mymoney-devpleno2.firebaseio.com/'
-
-const { useGet, usePost, useDelete } = Rest(baseURL)
+import Home from './pages/Home/'
+import Movimentacoes from './pages/Movimentacoes'
 
 function App() {
-
-  // const data = useGet('movimentacoes/2019-08')
-  const data = useGet('meses')
-  // const [postData, post] = usePost('movimentacoes/2019-08')
-  // const [deleteData, remove] = useDelete()
-  const saveNew = () => {
-    //   post({ valor: 10, descricao: "olá" })
-  }
-  const doRemove = () => {
-    //   remove('movimentacoes/2019-08/-LnDiS_P_AC8Kj210FvF')
-  }
-
   return (
-    <div>
-      <Header />
-      <div className='container'>
-        <AdicionarMes />
-        <Meses />
+    <Router>
+      <div>
+        <Header />
+        <Route path='/' exact component={Home} />
+        <Route path='/movimentacoes/:data' exact component={Movimentacoes} />
       </div>
-    </div>
+    </Router>
   )
 }
 
--- src/pages/Home/Meses.js
+++ src/pages/Home/Meses.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import Rest from './rest'
+import { Link } from 'react-router-dom'
+import Rest from '../../utils/rest'
 
 const baseURL = 'https://mymoney-devpleno2.firebaseio.com/'
-const { useGet} = Rest(baseURL)
+const { useGet } = Rest(baseURL)
 
 
 const Meses = () => {
@@ -30,7 +31,7 @@ const Meses = () => {
                             .map(mes => {
                                 return (
                                     <tr key={mes}>
-                                        <td>{mes}</td>
+                                        <td><Link to={`/movimentacoes/${mes}`}> {mes}</Link></td>
                                         <td>{data.data[mes].previsao_entrada}</td>
                                         <td>{data.data[mes].entrada}</td>
                                         <td>{data.data[mes].previsao_saida}</td>
