011bfbb2bbea905724c7109a97a8f3fd1b36aee2
Ikkyy <msndoikky@hotmail.com>
2019-09-02 16:32:32 -0300

    Adicionar e Remover Movimentações

--- src/pages/Movimentacoes.js
+++ src/pages/Movimentacoes.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Rest from '../utils/rest'
 
 const baseURL = 'https://mymoney-devpleno2.firebaseio.com/'
-const { useGet } = Rest(baseURL)
+const { useGet, usePost, useDelete } = Rest(baseURL)
 
 
 const Movimentacoes = ({ match }) => {
     const data = useGet(`movimentacoes/${match.params.data}`)
+    const [postData, salvar] = usePost(`movimentacoes/${match.params.data}`)
+    const [removeData, remover] = useDelete('')
+    const [descricao, setDescricao] = useState('')
+    const [valor, setValor] = useState(0.0)
+
+    const onChangeDescricao = evt => {
+        setDescricao(evt.target.value)
+    }
+
+    const onChangeValor = evt => {
+        setValor(parseFloat(evt.target.value))
+    }
+
+    const salvarMovimentacao = async () => {
+        await salvar({
+            descricao,
+            valor
+        })
+        setDescricao('')
+        setValor(0)
+        data.refetch()
+    }
+    const removerMovimentacao = async (id) => {
+        await remover(`movimentacoes/${match.params.data}/${id}`)
+        data.refetch()
+    }
+
     return (
         <div className='container'>
             <h1>Movimentações</h1>
@@ -26,10 +53,18 @@ const Movimentacoes = ({ match }) => {
                                     <tr>
                                         <td>{data.data[movimentacao].descricao}</td>
                                         <td>{data.data[movimentacao].valor}</td>
+                                        <button onClick={() => removerMovimentacao(movimentacao)}>-</button>
                                     </tr>
                                 )
                             })
                     }
+                    <tr>
+                        <td><input type='text' value={descricao} onChange={onChangeDescricao} /></td>
+                        <td><input type='text' value={valor} onChange={onChangeValor} />
+                            <button onClick={salvarMovimentacao}>+</button>
+                        </td>
+
+                    </tr>
 
                 </tbody>
             </table>
