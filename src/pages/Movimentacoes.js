c457e2406f9bb3c9805a7b706aa38087230c802e
Ikkyy <msndoikky@hotmail.com>
2019-09-02 16:40:20 -0300

    Fix NaN

--- src/pages/Movimentacoes.js
+++ src/pages/Movimentacoes.js
@@ -10,24 +10,27 @@ const Movimentacoes = ({ match }) => {
     const [postData, salvar] = usePost(`movimentacoes/${match.params.data}`)
     const [removeData, remover] = useDelete('')
     const [descricao, setDescricao] = useState('')
-    const [valor, setValor] = useState(0.0)
+    const [valor, setValor] = useState('')
 
     const onChangeDescricao = evt => {
         setDescricao(evt.target.value)
     }
 
     const onChangeValor = evt => {
-        setValor(parseFloat(evt.target.value))
+        setValor(evt.target.value)
     }
 
     const salvarMovimentacao = async () => {
-        await salvar({
-            descricao,
-            valor
-        })
-        setDescricao('')
-        setValor(0)
-        data.refetch()
+        if (!isNaN(valor) && valor.search(/^[-]?\d+(\.)?\d+?$/) >= 0) {
+            await salvar({
+                descricao,
+                valor: parseFloat(valor)
+            })
+            setDescricao('')
+            setValor(0)
+            data.refetch()
+        }
+
     }
     const removerMovimentacao = async (id) => {
         await remover(`movimentacoes/${match.params.data}/${id}`)
@@ -50,18 +53,18 @@ const Movimentacoes = ({ match }) => {
                             .keys(data.data)
                             .map(movimentacao => {
                                 return (
-                                    <tr>
+                                    <tr key={movimentacao}>
                                         <td>{data.data[movimentacao].descricao}</td>
-                                        <td>{data.data[movimentacao].valor}</td>
-                                        <button onClick={() => removerMovimentacao(movimentacao)}>-</button>
+                                        <td className='text-right'>{data.data[movimentacao].valor} {''}</td>
+                                        <button className='btn btn-danger ml-2' onClick={() => removerMovimentacao(movimentacao)}>-</button>
                                     </tr>
                                 )
                             })
                     }
                     <tr>
                         <td><input type='text' value={descricao} onChange={onChangeDescricao} /></td>
-                        <td><input type='text' value={valor} onChange={onChangeValor} />
-                            <button onClick={salvarMovimentacao}>+</button>
+                        <td><input type='text' value={valor} onChange={onChangeValor}  />
+                            <button className='btn btn-success ml-2' onClick={salvarMovimentacao}>+</button>
                         </td>
 
                     </tr>
