89df3889cde0dd1dae6fd06e58b429608fdfbcf9
Ikkyy <msndoikky@hotmail.com>
2019-09-04 19:56:53 -0300

    Autenticacao

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,24 +7,33 @@ const url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPasswo
 const Login = () => {
     const [postData, signin] = usePost(url)
     const [logado, setLogado] = useState(false)
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
     useEffect(() => {
         if (Object.keys(postData.data).length > 0) {
             localStorage.setItem('token', postData.data.idToken)
             window.location.reload()
         }
     }, [postData])
-    useEffect(()=>{
+    useEffect(() => {
         const token = localStorage.getItem('token')
-        if(token){
+        if (token) {
             setLogado(true)
         }
     })
     const login = async () => {
         await signin({
-            email: "msndoikky@hotmail.com",
-            password: "123456",
+            email,
+            password: senha,
             returnSecureToken: true
         })
+    }
+
+    const onChangeEmail = evt => {
+        setEmail(evt.target.value)
+    }
+    const onChangeSenha = evt => {
+        setSenha(evt.target.value)
     }
 
     if (logado) {
@@ -34,6 +43,13 @@ const Login = () => {
     return (
         <div className='container'>
             <h1>Login</h1>
+            {
+                postData.error && postData.error.length > 0 &&
+                <p>Email e/ou senha inválidos.</p>
+            }
+            <pre>{JSON.stringify(postData)}</pre>
+            <input type='text' value={email} placeholder='Seu email' onChange={onChangeEmail} />
+            <input type='password' value={senha} placeholder='Sua senha' onChange={onChangeSenha} />
             <button onClick={login}>Login</button>
         </div>
     )
